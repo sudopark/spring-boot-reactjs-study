0b9e924ce3508b45991cdb77b5efb84383235a09
sudo.park <sudo.park@kakao.com>
2022-03-20 22:13:33 +0900

    add entity and dto

---
+++ src/main/java/com/example/demo/models/TodoDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+
+	private String id;
+	private String title;
+	private boolean isDone;
+	
+	public TodoDTO(final TodoEntity entity) {
+		this();
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.isDone = entity.isDone();
+	}
+}
---
+++ src/main/java/com/example/demo/models/TodoEntity.java
@@ -0,0 +1,9 @@
+package com.example.demo.models;
+
+public class TodoEntity {
+
+	private String id;
+	private String userID;
+	private String title;
+	private boolean isDone;
+}
